6f5b503ca03aa3a6b15bacd9977818207d941768
amrazek <adam.mrazel@mgm-tp.com>
2017-05-10 21:56:04 +0200

    * Workouts - POST * Create new workout

--- application/controllers/api/User_Api.php
+++ application/controllers/api/User_Api.php
@@ -73,7 +73,6 @@ class User_Api extends REST_Controller
         $user = $this->em->getRepository(USER)->find($id);
         $workouts = $user->getWorkouts();
 
-
         /***
          * @var \Entity\Workout $workout
          */
@@ -88,6 +87,62 @@ class User_Api extends REST_Controller
         }
 
         $this->set_response($response, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+    }
+
+
+    /***
+     * Create new workout for current user and schedule it if Date is passed in
+     */
+    public function workouts_post()
+    {
+        $id = $this->get('id');
+        if (empty($id)) {
+            $this->set_response(['message' => 'Specify user id or nickname'], REST_Controller::HTTP_UNAUTHORIZED);
+        }
+
+        /**
+         * @var Entity\User $user
+         */
+        $user = $this->em->getRepository(USER)->find($id);
+
+        $name = $this->post('name');
+        $date = $this->post('date');
+        $isSuperset = $this->post('isSuperset');
+        $date = new DateTime($date);
+        $exerciseList = $this->post('exerciseList');
+
+        $workout = new \Entity\Workout();
+        $workout->setName($name);
+        $workout->addUser($user);
+
+
+        $workoutSchedule = new \Entity\WorkoutSchedule();
+        $workoutSchedule->setDate($date);
+        $workoutSchedule->setWorkout($workout);
+        $workoutSchedule->setUser($user);
+
+        foreach ($exerciseList as $exerciseRaw) {
+            /**
+             * @var Entity\Exercise $exercise
+             */
+            $exercise = $this->em->getRepository(EXERCISE)->find($exerciseRaw['id']);
+            $workout->addExercise($exercise);
+            $workoutScheduleExercise = new \Entity\WorkoutScheduleExercise();
+            $workoutScheduleExercise->setWorkoutSchedule($workoutSchedule);
+            $workoutScheduleExercise->setExercise($exercise);
+
+            $this->em->persist($workoutScheduleExercise);
+            $workoutSchedule->addWorkoutScheduleExercise($workoutScheduleExercise);
+        }
+
+
+        $workout->addWorkoutSchedule($workoutSchedule);
+
+        $this->em->persist($workoutSchedule);
+        $this->em->persist($workout);
+        $this->em->flush();
+
+        $this->set_response(['message' => 'Workout created', 'date' => $date], REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
     }
 
     public function history_get()
